ecf3a8d0085e684fad45768beccbc18b6f39a7f1
Isaac Martin <hey@isaacmartin.co>
2022-09-12 13:33:12 -0400

    add referenced by

--- deskStructure.js
+++ deskStructure.js
@@ -1,5 +1,10 @@
 import S from '@sanity/desk-tool/structure-builder';
 import {orderableDocumentListDeskItem} from '@sanity/orderable-document-list';
+import {ReferencedByView} from 'part:@indent-oss/sanityio-referenced-by';
+
+export const getDefaultDocumentNode = () => {
+  return S.document().views([S.view.form(), S.view.component(ReferencedByView).title('Referenced by')]);
+};
 
 export default () =>
   S.list()
